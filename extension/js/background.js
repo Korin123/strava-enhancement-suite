7c837deb42f766a10b9f08464d9a3961b00024b3
Korin123 <36076423+Korin123@users.noreply.github.com>
2025-06-21 15:27:29 +0100

    feat: migrate extension to manifest v3

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,7 +1,13 @@
 /* global chrome */
 
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable();
+});
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.indexOf('.strava.com') > -1) {
-    chrome.pageAction.show(tabId);
+  if (tab.url && tab.url.indexOf('.strava.com') > -1) {
+    chrome.action.enable(tabId);
+  } else {
+    chrome.action.disable(tabId);
   }
 });
